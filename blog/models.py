d7dc5fcd50ed259a3b985f649802a5ea4a4d2ef7
Pasindu sadanjana <pasindusadanjana17@gmail.com>
2026-05-19 13:14:57 +0530

    blog each image

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
     content = models.TextField()
     image = CloudinaryField('image')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title

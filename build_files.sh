8e12f1448582bddcc119fabb326800dff2dd83a7
Pasindu sadanjana <pasindusadanjana17@gmail.com>
2026-05-20 10:40:12 +0530

    build_fils

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing dependencies..."
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
